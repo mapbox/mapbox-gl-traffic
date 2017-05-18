e7ccdf74695832f30a68cb616a863a05a368965c
Lukas Martinelli <me@lukasmartinelli.ch>
2017-05-18 17:47:48 -0400

    Remove dist folder

--- mapbox-gl-traffic.js
+++ mapbox-gl-traffic.js
@@ -1,4 +1,3 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.MapboxTraffic = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * Create a new [Mapbox GL JS plugin](https://www.mapbox.com/blog/build-mapbox-gl-js-plugins/) that allows you to hide and show
  * traffic layers in your map and an optional toggle button.
@@ -119,7 +118,8 @@ TrafficButton.prototype.setMapIcon = function () {
   this._btn.className = 'mapboxgl-ctrl-icon mapboxgl-ctrl-map';
 };
 
-module.exports = MapboxTraffic;
-
-},{}]},{},[1])(1)
-});
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = MapboxTraffic;
+} else {
+  window.MapboxTraffic = MapboxTraffic;
+}
